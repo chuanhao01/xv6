02e1a20b5a7d0ea56102128ae90932e8090f1ff5
chuanhao01 <34702921+chuanhao01@users.noreply.github.com>
2025-06-14 00:33:29 +0800

    [feat] add python pkgs to dev flake

--- flake.nix
+++ flake.nix
@@ -19,6 +19,10 @@
           system = "riscv64-linux"; # The target system
         };
       };
+      # Pulling lib into outputs
+      inherit (nixpkgs) lib;
+      # Use Python 3.12 from nixpkgs
+      python = pkgs.python312;
     in
     {
       devShells.${system} = {
@@ -37,10 +41,26 @@
             man-pages
             qemu
             gcc # We need original gcc?
+            python
+            uv
           ];
+
+          env =
+            {
+              # Prevent uv from managing Python downloads
+              UV_PYTHON_DOWNLOADS = "never";
+              # Force uv to use nixpkgs Python interpreter
+              UV_PYTHON = python.interpreter;
+            }
+            // lib.optionalAttrs pkgs.stdenv.isLinux {
+              # Python libraries often load native shared objects using dlopen(3).
+              # Setting LD_LIBRARY_PATH makes the dynamic library loader aware of libraries without using RPATH for lookup.
+              LD_LIBRARY_PATH = lib.makeLibraryPath pkgs.pythonManylinuxPackages.manylinux1;
+            };
 
           # Optional: set environment variables if your build system expects them
           shellHook = ''
+            unset PYTHONPATH
             echo "Entering RISC-V cross-compilation dev shell."
           '';
 
